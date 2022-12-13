098f282ade9db778a9fd96b99a15ea92aa504948
Chiamaka Ikeanyi <chiamakaikeanyi@gmail.com>
2022-12-13 15:31:11 -0800

    Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,16 +6,15 @@
  *
  * Return: 0 (Success)
  */
-
 int main(void)
 {
 char text[9] = "_putchar";
 int i = 0;
-for (i = 0; i < 8; i++)
+for(i = 0; i < 8; i++)
 {
-  _putchar(text[i]);
+_putchar(text[i]);
 }
- _putchar('\n');
+_putchar('\n');
 
- return (0);
+return (0);
 }
